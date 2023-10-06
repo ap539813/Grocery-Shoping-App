64adbcb9f47ed8433d724cca1746a73fd6b9d144
Ankush Pandey <ap539813@gmail.com>
2023-10-06 14:08:12 +0530

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePageComponent from './components/HomePageComponent.vue'; 
+import HomePageComponent from './components/HomePageComponent.vue';
+import RegisterPage from './components/RegisterPage.vue';
+import UserLoginPage from './components/UserLoginPage.vue';
+// import ManagerLoginPage from './components/ManagerLoginPage.vue';
+// import AdminLoginPage from './components/AdminLoginPage.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomePageComponent
-  }
+  { path: '/', name: 'Home', component: HomePageComponent },
+  { path: '/register', name: 'Register', component: RegisterPage },
+  { path: '/login/user', name: 'LoginUser', component: UserLoginPage },
+  // { path: '/login/manager', name: 'LoginManager', component: ManagerLoginPage },
+  // { path: '/login/admin', name: 'LoginAdmin', component: AdminLoginPage }
 ];
 
 const router = createRouter({
