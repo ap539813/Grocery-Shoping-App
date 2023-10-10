e1a7be7e1966c6ce998630b308c37dee1dd46814
Ankush Pandey <ap539813@gmail.com>
2023-10-10 11:37:56 +0530

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,15 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePageComponent from './components/HomePageComponent.vue';
 import RegisterPage from './components/RegisterPage.vue';
 import UserLoginPage from './components/UserLoginPage.vue';
-// import ManagerLoginPage from './components/ManagerLoginPage.vue';
-// import AdminLoginPage from './components/AdminLoginPage.vue';
+import ManagerLoginPage from './components/ManagerLoginPage.vue';
+import AdminLoginPage from './components/AdminLoginPage.vue';
+import AdminDashboard from './components/AdminDashboard.vue';
+
 
 const routes = [
   { path: '/', name: 'Home', component: HomePageComponent },
   { path: '/register', name: 'Register', component: RegisterPage },
   { path: '/login/user', name: 'LoginUser', component: UserLoginPage },
-  // { path: '/login/manager', name: 'LoginManager', component: ManagerLoginPage },
-  // { path: '/login/admin', name: 'LoginAdmin', component: AdminLoginPage }
+  { path: '/login/manager', name: 'LoginManager', component: ManagerLoginPage },
+  { path: '/login/admin', name: 'LoginAdmin', component: AdminLoginPage },
+  { path: '/admin-dashboard', name: 'AdminDashboard', component: AdminDashboard}
 ];
 
 const router = createRouter({
