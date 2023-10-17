073d3816226b786c1e7d5cc040d02b36d07cb963
Ankush Pandey <ap539813@gmail.com>
2023-10-17 19:48:11 +0530

    Update RegisterPage.vue

--- frontend/src/components/RegisterPage.vue
+++ frontend/src/components/RegisterPage.vue
@@ -35,6 +35,7 @@ export default {
 
               let data = await response.json();
               alert(data.message);
+
               
               this.username = '';
               this.password = '';
