15e51731c9cd38424759a1b5084b42b148d3baad
Ankush Pandey <ap539813@gmail.com>
2023-11-28 10:30:46 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp, reactive } from 'vue';
 import App from './App.vue';
 import router from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const app = createApp(App)
   .use(router)
