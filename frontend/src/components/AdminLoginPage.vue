e60f81d43bc85b74ac3df9ca52d6b5fbff76efe6
Ankush Pandey <ap539813@gmail.com>
2023-11-28 10:27:10 +0530

    Update AdminLoginPage.vue

--- frontend/src/components/AdminLoginPage.vue
+++ frontend/src/components/AdminLoginPage.vue
@@ -1,19 +1,23 @@
 <template>
     <!-- Navigation Bar -->
-    <nav class="navbar">
-      <div class="navbar-left">
-        <span id="manager-username">Login Admin</span>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="navbar-brand">
+        <span>Login Admin</span>
       </div>
-      <div class="navbar-right">
-        <button @click="goToHome">Home</button>
+      <div class="ml-auto">
+        <button class="btn btn-outline-primary" @click="goToHome">Home</button>
       </div>
     </nav>
-    <!-- <h1>Admin Login</h1> -->
-  <div class="user-login-page">
-    <input type="text" v-model="username" placeholder="Username" />
-    <input type="password" v-model="password" placeholder="Password" />
-    <button @click="login">Login Admin</button>
-  </div>
+  
+    <div class="container mt-5">
+      <div class="form-group">
+        <input type="text" class="form-control" v-model="username" placeholder="Username" />
+      </div>
+      <div class="form-group">
+        <input type="password" class="form-control" v-model="password" placeholder="Password" />
+      </div>
+      <button class="btn btn-primary" @click="login">Login Admin</button>
+    </div>
 </template>
 
 <script>
@@ -63,10 +67,81 @@ export default {
 
 <style scoped>
 /* Add styling for the login page here */
-.user-login-page {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
+body {
+  font-family: 'Arial', sans-serif;
+  background-color: #f5f5f5;
+  text-align: center;
+  margin-top: 50px;
 }
+
+.container {
+  background-color: white;
+  padding: 20px 30px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  width: 90%;
+  max-width: 400px;
+  margin: 0 auto;
+  border-radius: 8px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: 600;
+}
+
+.form-group input {
+  width: 100%;
+  padding: 10px 15px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  box-sizing: border-box;
+}
+
+/* Bootstrap styles will override these, so adjust or remove as needed */
+button {
+  background-color: #008CBA;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 4px;
+  transition: background-color 0.2s;
+}
+
+button:hover {
+  background-color: #006c99;
+}
+
+.navbar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+            background-color: #333;
+            color: white;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+            position: fixed; 
+            top: 0; 
+            left: 0;  
+            width: 100%; 
+            z-index: 1000;
+            box-sizing: border-box; 
+            padding-right: 15px;
+        }
+
+        .navbar-left, .navbar-right {
+            display: flex;
+            align-items: center;
+        }
 </style>
